1924de8980ba5318df4fe8d22a72ad6b0e52ce0f
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2026-02-24 15:26:06 +0530

    Add user schema for MongoDB with validation and default values

---
+++ src/models/UserSchema.ts
@@ -0,0 +1,94 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required : true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phoneNumber:{
+        type: Number,
+    },
+    isEmailVerified:{
+        type: Boolean,
+        required: true,
+        default:"false"
+    },
+    isPhoneVerified:{
+        type: Boolean,
+        required: true,
+        default:"false"
+    },
+    authProvider:{
+        type: String,
+        enum:["google","local"],
+        default: "local"
+    },
+    name:{
+        type: String,
+    },
+    profilePic:{
+        type: Image,
+    },
+    bio:{
+        type:String,
+        default:"Hey There! I'm using Tea-Chat"
+    },
+    accountType:{
+        type: String,
+        enum: ["public", "private"],
+        required: true
+    },
+    isOnline:{
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    lastSeen:{
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    readReceipt:{
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    lastSeenVisibility:{
+        type: String,
+        enum: ["everyone" , "connections" , "nobody"],
+        default: "everyone",
+        required: true
+    },
+    profilePhotoVisibility:{
+        type: String,
+        enum: ["everyone" , "connections" , "nobody"],
+        required: true,
+        default: "everyone"
+    },
+    statusVisibility:{
+        type: String,
+        enum: ["everyone" , "connections"],
+        required: true,
+        default: "everyone"
+    },
+    accountStatus:{
+        type: String,
+        enum: ["active","suspended","deleted"],
+        required: true,
+        default: "active"
+    }
+},{
+    timestamps: true
+});
+
+export default mongoose.model("Users", userSchema);
